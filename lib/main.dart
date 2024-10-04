9dc6c0ca81e540fc1844a5ec2b3cecfec8f2be4b
Suhas Bharadwaj <suhas.b@technovert.com>
2024-10-04 15:59:59 +0530

    implement callback for media share selection

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,10 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: AppList(),
+        body: SafeArea(
+          child: Center(
+            child: AppList(),
+          ),
         ),
       ),
     );
--- lib/widgets/app_list.dart
+++ lib/widgets/app_list.dart
@@ -1,13 +1,37 @@
+import 'dart:async';
+
 import 'package:clipboard_privacy/models/monitored_app.dart';
 import 'package:flutter/material.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
-class AppList extends StatelessWidget {
+class AppList extends StatefulWidget {
+  const AppList({super.key});
+
+  @override
+  State<AppList> createState() => _AppListState();
+}
+
+class _AppListState extends State<AppList> {
+  late StreamSubscription _intentSub;
+
   final List<MonitoredApp> appList = [
     MonitoredApp(1, "Twitter", "twitter", ["t", "s"], true),
     MonitoredApp(2, "Instagram", "instagram", ["igsh"], true)
   ];
 
-  AppList({super.key});
+  @override
+  void initState() {
+    super.initState();
+    // For sharing images coming from outside the app
+    // while the app is in the memory
+    _intentSub = ReceiveSharingIntent.instance.getMediaStream().listen(
+        (List<SharedMediaFile> media) {
+      debugPrint("Inside callback");
+      print(media.toList());
+    }, onError: (err) {
+      debugPrint("$err");
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,5 +47,11 @@ class AppList extends StatelessWidget {
               ))
           .toList(),
     );
+  }
+
+  @override
+  void dispose() {
+    _intentSub.cancel();
+    super.dispose();
   }
 }
